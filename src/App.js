11fcd717509aa076773f79b6047bc9c6dafcd5c9
Jam52 <jamie.sajdak@gmail.com>
2020-11-20 21:01:33 -0500

    Install node sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div data-testid="component-app"></div>;
+  return (
+    <div data-testid="component-app">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
