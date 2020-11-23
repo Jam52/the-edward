4d6708b59a177c51fedd0e7ba883a3b0dce41acd
sophieatgithub <sophie@bajescu.com>
2020-11-23 23:04:22 +0000

    wip

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import LandingPage from './views/LandingPage/LandingPage';
+import HousePage from './views/HousePage/HousePage';
 import styles from './App.module.scss';
 
 function App() {
   return (
     <div className={styles.page}>
       <div data-testid="component-app" className={styles.page_container}>
-        <LandingPage />
+        <HousePage />
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+// import React from 'react';
+// import LandingPage from './views/LandingPage/LandingPage';
+// import styles from './App.module.scss';
+
+// function App() {
+//   return (
+//     <div className={styles.page}>
+//       <div data-testid="component-app" className={styles.page_container}>
+//         <LandingPage />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
