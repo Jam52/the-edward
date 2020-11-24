69b0f68b7c9fb690c34f3cddbf5c37f61c4f6027
Jam52 <jamie.sajdak@gmail.com>
2020-11-24 11:33:12 -0500

    Add prop types to Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './Button.module.scss';
 
@@ -10,6 +11,11 @@ const Button = (props) => {
       </Link>
     </div>
   );
+};
+
+Button.propTypes = {
+  to: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default Button;
