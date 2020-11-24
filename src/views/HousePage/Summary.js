4f3cf00201cc2c7dfe382c376a6cfbbd14960d5f
sophieatgithub <sophie@bajescu.com>
2020-11-24 13:57:57 +0000

    less than 300px CSS

--- src/views/HousePage/Summary.js
+++ src/views/HousePage/Summary.js
@@ -6,15 +6,15 @@ const Summary = () => {
     return (
     <div className={styles.summaryContainer}>
       <div className={styles.innerContainerOne}>
-        <section>
+        <div>
           <h1>Welcome to your home away from home.</h1>
           <p>3 Suites. 3 Baths. 2 Living Spaces.</p>
           <p>1 Dining Room. 1 Solarium</p>
-        </section>
+        </div>
         <Button className={styles.bookButton} text ={"Book House"}></Button>
       </div>
       <div className={styles.innerContainerTwo}>
-        <p>
+        <p className={styles.description}>
           With 24 ft high atrium ceilings and three grand suites this light filled two-
           storey house is ideal for families or a group of friends. Much love, creativity
           and attention to detail has gone into curating a space that balances classic
@@ -23,6 +23,7 @@ const Summary = () => {
         <h4>Max Occupancy</h4>
         <p>6 Adults</p>
         <p>3 Children under 10</p>
+        <br></br>
         <h4>Rate</h4>
         <p>From From $675/night + Cleaning Fee</p>
       </div>
