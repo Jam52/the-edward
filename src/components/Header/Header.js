20491e6889995c6d6ad5ccd3ac17e0f65856ea9c
Jam52 <jamie.sajdak@gmail.com>
2020-11-25 09:16:18 -0500

    Add navitem styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 import NavList from '../NavList/NavList';
 
 const Header = () => {
   return (
     <header data-testid="component-header" className={styles.header}>
-      <h1 className={styles.header_title}>The Edward</h1>
+      <h1 className={styles.header_title}>
+        <Link to="/">The Edward</Link>
+      </h1>
+
       <NavList />
     </header>
   );
--- src/components/NavList/NavItem/NavItem.js
+++ src/components/NavList/NavItem/NavItem.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styles from './NavLitem.module.scss';
 
 const NavItem = (props) => {
-  return <NavLink to={props.to}>{props.text}</NavLink>;
+  return (
+    <NavLink
+      to={props.to}
+      className={styles.nav}
+      activeClassName={styles.active}
+    >
+      {props.text}
+    </NavLink>
+  );
 };
 
 NavLink.propTypes = {
