5db5290b575a6835babfa342cfc7d9fb25e17a45
Jam52 <jamie.sajdak@gmail.com>
2020-11-26 10:21:38 -0500

    Fix imageContainer, add ci= npm run build for netlify

--- src/components/Footer/__tests__/Footer.test.js
+++ src/components/Footer/__tests__/Footer.test.js
@@ -1,7 +1,17 @@
 import Footer from '../Footer';
 import { render } from '@testing-library/react';
 
-test('renders TheEdward header', () => {
+test('renders TheEdward footer', () => {
   const { getByTestId } = render(<Footer />);
   expect(getByTestId('component-footer')).toBeInTheDocument();
+});
+
+test('renders contact', () => {
+  const { getByText } = render(<Footer />);
+  expect(getByText('contact')).toBeInTheDocument();
+});
+
+test('renders address', () => {
+  const { getByText } = render(<Footer />);
+  expect(getByText('adress')).toBeInTheDocument();
 });
