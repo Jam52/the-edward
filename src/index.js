b6ce21fd0b934147d1f9ffb92981736b4ae51b41
Jam52 <jamie.sajdak@gmail.com>
2021-02-13 15:21:28 -0500

    Switch to use Browser router

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import './CSS/utility.scss';
 import './CSS/components.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
